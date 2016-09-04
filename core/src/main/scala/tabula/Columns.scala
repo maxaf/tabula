0572dbca6ac250d3be1d183fa545f4ba935eade4
Max Afonov <max@bumnetworks.com>
2016-09-04 02:54:59 -0400

    expose row() function off columns

--- core/src/main/scala/tabula/Columns.scala
+++ core/src/main/scala/tabula/Columns.scala
@@ -10,11 +10,14 @@ abstract class Columns[F, T, C, Tail <: HList, O <: HList](val columns: Column[F
     aa: ApplyAll[F, Column[F, T, C] :: Tail, ColumnAndCell[F, T, C] :: O]
 ) {
   private val cellsF = cells(columns)
+  def row[Fmt <: Format](fmt: Fmt)(x: F)(
+    implicit
+    lf: LeftFolder[ColumnAndCell[F, T, C] :: O, fmt.Row, fmt.type]
+  ) = cellsF(x).row(fmt)
+  def writer[Fmt <: Format](fmt: Fmt)(f: fmt.Factory => fmt.Writer) = f(fmt.writer(columns))
   def write[Fmt <: Format](fmt: Fmt)(f: fmt.Factory => fmt.Writer)(xs: Iterator[F])(
     implicit
     lf: LeftFolder[ColumnAndCell[F, T, C] :: O, fmt.Row, fmt.type]
   ) =
-    writer(fmt)(f).write(xs.map(cellsF(_).row(fmt)))
-  def writer[Fmt <: Format](fmt: Fmt)(f: fmt.Factory => fmt.Writer) =
-    f(fmt.writer(columns))
+    writer(fmt)(f).write(xs.map(row(fmt)(_)))
 }
